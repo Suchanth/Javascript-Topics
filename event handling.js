c7c6149067ca3c547651bdf70b0424d8f3e78228
SUCHANTH B <115070004+Suchanth@users.noreply.github.com>
2024-02-02 15:17:59 +0530

    Add files via upload

---
+++ event handling.js
@@ -0,0 +1,11 @@
+//Onclick event
+//function changeImage(myImage){
+  //  document.querySelector("#box").style.backgroundColor=myImage;
+//}
+
+function changeImage(source){
+    document.querySelector('img').src=source;
+}
+function changeWallpaper(name){
+    document.querySelector('img').src=name;
+}
